ceca3dfeba50effd7871c25346de512679047f16
uehara1414 <b1015062@fun.ac.jp>
2016-11-01 20:02:16 +0900

    Modify run_playbook and added some functions.

--- ansible_subprocess/main.py
+++ ansible_subprocess/main.py
@@ -1,21 +1,53 @@
 import subprocess
 
-def run_playbook(playbook_filename, hosts, playbook_cmd='ansible', private_key=None, **vars):
 
+def run_playbook(playbook_filename, hosts, playbook_cmd='ansible-playbook', private_key=None, **extra_vars):
+
+    command = construct_playbook_command(playbook_filename, hosts, playbook_cmd, private_key, extra_vars)
+
+    process = subprocess.Popen(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    return (process.wait(), process.stdout.read(), process.stderr.read())
+
+
+def construct_playbook_command(playbook_filename, hosts, playbook_command='ansible-playbook', private_key=None, extra_vars=None):
     hosts = ",".join(hosts) + ","
 
     command = [
-        playbook_cmd,
+        playbook_command,
         playbook_filename,
         '-i',
         hosts,
     ]
 
-    if vars:
-        extra_vars = ' '.join("{}={}".format(key, value) for (key, value) in vars.keys())
-        command += ['--extra-vars', extra_vars]
+    if extra_vars:
+        vars = ' '.join("{}={}".format(key, value) for (key, value) in extra_vars.keys())
+        command.extend(['--extra-vars', vars])
 
-    process = subprocess.Popen(command, stderr=subprocess.PIPE)
+    return command
 
-    return (process.wait(), process.stderr.read())
 
+def run_ping(host):
+
+    command = [
+        'ansible',
+        host,
+        '-m',
+        'ping'
+    ]
+
+    process = subprocess.Popen(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    return (process.wait(), process.stdout.read(), process.stderr.read())
+
+
+def construct_ping_command(host, ansible_command):
+
+    command = [
+        ansible_command,
+        host,
+        '-m',
+        'ping'
+    ]
+
+    return command
