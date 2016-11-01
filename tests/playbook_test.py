f7539d87c73033c71bff767f807021448537bcf3
uehara1414 <b1015062@fun.ac.jp>
2016-11-01 20:44:34 +0900

    Added more tests.

--- tests/playbook_test.py
+++ tests/playbook_test.py
@@ -16,6 +16,18 @@ class CommandTest(unittest.TestCase):
                 ]
         )
 
+    def test_construct_playbook_command_with_host_name(self):
+        command = construct_playbook_command('tests/playbooks/simple-playbook.yml', 'hosts')
+        self.assertEqual(
+            command,
+            [
+                'ansible-playbook',
+                'tests/playbooks/simple-playbook.yml',
+                '-i',
+                'hosts'
+            ]
+        )
+
     def test_construct_playbook_command_with_specific_ansible_command(self):
         command = construct_playbook_command('tests/playbooks/simple-playbook.yml', ['127.0.0.1'], playbook_command='/bin/ansible-playbook')
         self.assertEqual(
@@ -62,7 +74,21 @@ class CommandTest(unittest.TestCase):
                 'ansible-playbook',
                 'tests/playbooks/simple-playbook.yml',
                 '-i',
-                '127.0.0.1,'
+                '127.0.0.1,',
                 '--private-key=/home/foo/id_rsa'
+                ]
+        )
+
+    def test_construct_playbook_command_with_extra_options(self):
+        command = construct_playbook_command('tests/playbooks/simple-playbook.yml', ['127.0.0.1'], private_key='/home/foo/id_rsa', extra_options='--syntax-check')
+        self.assertEqual(
+            command,
+            [
+                'ansible-playbook',
+                'tests/playbooks/simple-playbook.yml',
+                '-i',
+                '127.0.0.1,'
+                '--private-key=/home/foo/id_rsa',
+                '--syntax-check'
                 ]
         )
