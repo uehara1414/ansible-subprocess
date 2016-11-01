2241350bc7f0fb27c09637b55c1d2d3a4799aca4
uehara1414 <b1015062@fun.ac.jp>
2016-11-01 19:59:32 +0900

    Added tests.

---
+++ tests/playbook_test.py
@@ -0,0 +1,68 @@
+import unittest
+from ansible_subprocess.main import construct_playbook_command
+
+
+class CommandTest(unittest.TestCase):
+
+    def test_construct_playbook_command(self):
+        command = construct_playbook_command('tests/playbooks/simple-playbook.yml', ['127.0.0.1'])
+        self.assertEqual(
+            command,
+            [
+                'ansible-playbook',
+                'tests/playbooks/simple-playbook.yml',
+                '-i',
+                '127.0.0.1,'
+                ]
+        )
+
+    def test_construct_playbook_command_with_specific_ansible_command(self):
+        command = construct_playbook_command('tests/playbooks/simple-playbook.yml', ['127.0.0.1'], playbook_command='/bin/ansible-playbook')
+        self.assertEqual(
+            command,
+            [
+                '/bin/ansible-playbook',
+                'tests/playbooks/simple-playbook.yml',
+                '-i',
+                '127.0.0.1,'
+                ]
+        )
+
+    def test_construct_playbook_command_with_two_hosts(self):
+        command = construct_playbook_command('tests/playbooks/simple-playbook.yml', ['127.0.0.1', '127.0.0.2'])
+        self.assertEqual(
+            command,
+            [
+                'ansible-playbook',
+                'tests/playbooks/simple-playbook.yml',
+                '-i',
+                '127.0.0.1,127.0.0.2,'
+                ]
+        )
+
+    def test_construct_playbook_command_with_extra_vars(self):
+        command = construct_playbook_command('tests/playbooks/simple-playbook.yml', ['127.0.0.1', '127.0.0.2'], {"extra1": "var1", "extra2": "var2"})
+        self.assertEqual(
+            command,
+            [
+                'ansible-playbook',
+                'tests/playbooks/simple-playbook.yml',
+                '-i',
+                '127.0.0.1,127.0.0.2,'
+                '--extra-vars',
+                '"extra1=var1 extra2=var2"'
+                ]
+        )
+
+    def test_construct_playbook_command_with_private_key(self):
+        command = construct_playbook_command('tests/playbooks/simple-playbook.yml', ['127.0.0.1'], private_key='/home/foo/id_rsa')
+        self.assertEqual(
+            command,
+            [
+                'ansible-playbook',
+                'tests/playbooks/simple-playbook.yml',
+                '-i',
+                '127.0.0.1,'
+                '--private-key=/home/foo/id_rsa'
+                ]
+        )
