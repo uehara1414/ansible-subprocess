e592ffa90520335af4144208f4ad3bd5cc3bb877
uehara1414 <b1015062@fun.ac.jp>
2016-11-01 20:50:41 +0900

    Updated functions.

--- ansible_subprocess/main.py
+++ ansible_subprocess/main.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def run_playbook(playbook_filename, hosts, playbook_cmd='ansible-playbook', private_key=None, **extra_vars):
+def run_playbook(playbook_filename, hosts, playbook_cmd='ansible-playbook', private_key=None, extra_options=None, **extra_vars):
 
     command = construct_playbook_command(playbook_filename, hosts, playbook_cmd, private_key, extra_vars)
 
@@ -10,8 +10,13 @@ def run_playbook(playbook_filename, hosts, playbook_cmd='ansible-playbook', priv
     return (process.wait(), process.stdout.read(), process.stderr.read())
 
 
-def construct_playbook_command(playbook_filename, hosts, playbook_command='ansible-playbook', private_key=None, extra_vars=None):
-    hosts = ",".join(hosts) + ","
+def construct_playbook_command(playbook_filename, hosts, playbook_command='ansible-playbook', private_key=None, extra_options=None, extra_vars=None):
+
+    if isinstance(hosts, list):
+        hosts = ",".join(hosts) + ","
+
+    if not isinstance(hosts, str):
+        raise TypeError('hosts must be a str or list object')
 
     command = [
         playbook_command,
@@ -24,24 +29,29 @@ def construct_playbook_command(playbook_filename, hosts, playbook_command='ansib
         vars = ' '.join("{}={}".format(key, value) for (key, value) in extra_vars.keys())
         command.extend(['--extra-vars', vars])
 
+    if not extra_options:
+        return command
+
+    if isinstance(extra_options, str):
+        command.append(extra_options)
+    elif isinstance(extra_options, list):
+        command.extend(extra_options)
+    else:
+        raise TypeError("extra_options must be str or list.")
+
     return command
 
 
-def run_ping(host):
+def run_ping(host, ansible_command='ansible'):
 
-    command = [
-        'ansible',
-        host,
-        '-m',
-        'ping'
-    ]
+    command = construct_ping_command(host, ansible_command=ansible_command)
 
     process = subprocess.Popen(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     return (process.wait(), process.stdout.read(), process.stderr.read())
 
 
-def construct_ping_command(host, ansible_command):
+def construct_ping_command(host, ansible_command='ansible'):
 
     command = [
         ansible_command,
