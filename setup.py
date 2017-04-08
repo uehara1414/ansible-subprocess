12dd95df9e95d70318db4060ffaf119b57798fee
uehara <b1015062@fun.ac.jp>
2017-04-08 15:07:41 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ if os.path.exists("README.md"):
     longDesc = open("README.md").read().strip()
 
 setup(
-    name = "ansible-subprocess",
-    version = "0.1.0",
-    author = "uehara1414",
+    name="ansible-subprocess",
+    version="0.2.0",
+    author="uehara1414",
     author_email="",
-    description = ("Using Ansible from python subprocess."),
-    long_description = longDesc,
-    license = "MIT License",
-    keywords = "python Ansible subprocess",
-    url = "https://github.com/uehara1414/ansible-subprocess",
+    description=("Using Ansible from python subprocess."),
+    long_description=longDesc,
+    license="MIT License",
+    keywords="python Ansible subprocess",
+    url="https://github.com/uehara1414/ansible-subprocess",
     packages=['ansible_subprocess'],
     package_dir={'ansible_subprocess': 'ansible_subprocess'},
-    classifiers = [
+    classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
     ]
